51af015a7b03a21287665a535c842fbf586c44d2
caiosantosss <caio.santos@msn.com>
2023-02-16 15:26:08 +0900

    add checkScreen and togglemobilenavbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,7 +5,7 @@
         <router-link class="header" :to="{name: 'Home'}">FireBlogs</router-link>
       </div>
       <div class="nav-links">
-        <ul>
+        <ul v-show="!mobile">
           <router-link class="link" to="#">Home</router-link>
           <router-link class="link" to="#">Blogs</router-link>
           <router-link class="link" to="#">Create Post</router-link>
@@ -13,9 +13,9 @@
         </ul>
       </div>
     </nav>
-    <menuIcon class="menu-icon"/>
+    <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile"/>
     <transition name="mobile-nav">
-      <ul class="mobile-nav">
+      <ul class="mobile-nav" v-show="mobileNav">
         <router-link class="link" to="#">Home</router-link>
         <router-link class="link" to="#">Blogs</router-link>
         <router-link class="link" to="#">Create Post</router-link>
@@ -33,6 +33,32 @@ export default {
   components: {
     menuIcon,
   },
+  data() {
+    return {
+      mobile: null,
+      mobileNav: null,
+      windowWidth: null,
+    };
+  },
+  created() {
+    window.addEventListener('resize', this.checkScreen);
+    this.checkScreen();
+  },
+  methods: {
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+      this.mobileNav = false;
+      return;
+    },
+    toggleMobileNav() {
+      this.mobileNav = !this.mobileNav;
+    },
+  }
 }
 </script>
 
@@ -110,6 +136,11 @@ header {
     background-color: #303030;
     top: 0;
     left: 0;
+
+    .link {
+      padding: 15px 0;
+      color: #fff;
+    }
   }
 }
 </style>
