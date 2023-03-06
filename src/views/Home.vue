8f6a0a7261be55ca430e24cfaa119343208d78da
caiosantosss <caio.santos@msn.com>
2023-03-06 17:14:26 +0900

    add style to btn homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -74,10 +74,32 @@ export default {
     padding: 100px 25px;
     display: flex;
     flex-direction: column;
-    align-self: center;
+    align-items: center;
     @media (min-width: 768px) {
       padding: 125px 25px;
       flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 768px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 768px) {
+        text-align: initial;
+        font-size: 40px;
+      }
     }
   }
 }
