704a2a6f9a88698c2ce9a8273bc9e702b6b21d84
Caio Santos <caio.santos@msn.com>
2023-03-15 17:46:01 +0900

    add login inputs and button

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,47 @@
 <template>
-<div>
-LOGIN
-</div>
+  <div class="form-wrap">
+    <form class="login">
+      <p class="login-register">
+        Don't have an account?
+        <router-link class="router-link" :to="{name: 'Register'}">Register</router-link>
+      </p>
+      <h2>Login to FireBlogs</h2>
+      <div class="inputs">
+        <div class="input">
+          <input type="text" placeholder="Email" v-model="email">
+          <Email class="icon"/>
+        </div>
+        <div class="password">
+          <input type="text" placeholder="Password" v-model="password">
+          <Password class="icon"/>
+        </div>
+      </div>
+      <router-link class="forgot-password" :to="{name: 'ForgotPassword'}">Forgot your Password?</router-link>
+      <button>Sign In</button>
+      <div class="angle"></div>
+    </form>
+    <div class="background"></div>
+  </div>
 </template>
 
 <script>
+import Email from '../assets/Icons/envelope-regular.svg';
+import Password from '../assets/Icons/lock-alt-solid.svg';
 export default {
-
-}
+  name: "Login",
+  components: {
+    Email,
+    Password
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scope>
 
 </style>
