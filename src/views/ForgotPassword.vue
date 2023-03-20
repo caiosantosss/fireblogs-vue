b2012aa1a070d5c8f03713bb3b1eb58569035e12
Caio Santos <caio.santos@msn.com>
2023-03-20 18:59:43 +0900

    add forgotpassword

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,15 +1,47 @@
 <template>
-<div>
-FORGOT PASSWORD
-</div>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset Password</h2>
+        <p> Forgot your password? Enter your email below</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email">
+            <Email class="icon"/>
+          </div>
+        </div>
+        <button>Reset Password</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Email from '../assets/Icons/envelope-regular.svg';
 export default {
-
+name: "ForgotPassword",
+components: {
+  Email,
+},
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
 
 </style>
