8d5ae1e458c53af79627d03b36aef43d41c91d30
Caio Santos <caio.santos@msn.com>
2023-03-24 18:09:48 +0900

    add resetPassword function

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="reset-password">
-    <Modal v-if="modalActive" v-on:close-modal="closeModal" />
+    <Modal v-if="modalActive" :modalMessage="modalMessage" v-on:close-modal="closeModal" />
     <Loading v-if="loading" />
     <div class="form-wrap">
       <form class="reset">
+        <p class="login-register">
+          Back to
+          <router-link class="router-link" :to="{ name: 'Login' }">Login</router-link>
+        </p>
         <h2>Reset Password</h2>
         <p> Forgot your password? Enter your email below</p>
         <div class="inputs">
@@ -24,6 +28,9 @@
 import Email from '../assets/Icons/envelope-regular.svg';
 import Modal from '../components/Modal';
 import Loading from '../components/Loading';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 export default {
 name: "ForgotPassword",
 data() {
@@ -40,6 +47,20 @@ components: {
   Loading,
 },
 methods: {
+  resetPassword() {
+    this.loading = true;
+    firebase.auth().sendPasswordResetEmail(this.email)
+      .then(() => {
+        this.loading = false;
+        this.modalMessage = 'Password reset email sent!';
+        this.modalActive = true;
+      })
+      .catch((error) => {
+        this.loading = false;
+        this.modalMessage = error.message;
+        this.modalActive = true;
+      });
+  },
   closeModal() {
     this.modalActive = !this.modalActive;
     this.email = "";
