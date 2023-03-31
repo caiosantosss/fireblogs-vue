40955d04565870bffe926d1578af56ad6f69e717
Caio Santos <caio.santos@msn.com>
2023-03-31 18:44:51 +0900

    add template createpost

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,12 +1,47 @@
 <template>
-  <div>
-
+  <div class="create-post">
+    <div class="container">
+      <div :class="{ invisible: !error }" class="err-message">
+        <p><span>Error:</span>{{ this.errorMsg }}</p>
+      </div>
+      <div class="blog-info">
+        <input type="text" placeholder="Enter Blog Title" v-model="blogTitle">
+        <div class="upload-file">
+          <label for='blog-photo'>Upload Cover Photo</label>
+          <input type="file" ref="blog-photo" id="blog-photo" accept=".png, .jpg, jpeg" />
+          <button class="preview" :class="{'button-inactive': !this.$store.blogPhotoFileURL }">Preview Photo</button>
+          <span>File Chosen: {{ this.$store.state.blogPhotoName }}</span>
+        </div>
+      </div>
+      <div class="editor">
+        <vue-editor :editorOptions="editorSettings" v-model="blogHTML" useCustomImageHandler />
+      </div>
+      <div class="blog-actions">
+        <button>Publish Blog</button>
+        <router-link class="router-button" to="#" >Post Preview</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Quill from 'quill';
+window.Quill = Quill;
+const imageResize = require('quill-image-resize-module').default;
+Quill.register('modules/imageResize', imageResize);
 export default {
-
+  name: 'CreatePost',
+  data() {
+    return {
+      error: null,
+      errorMsg: null,
+      editorSettings: {
+        modules: {
+          imageResize: {},
+        }
+      }
+    }
+  }
 }
 </script>
 
