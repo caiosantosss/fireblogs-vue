f2ee73ef71b153339f03ae80e90ca2ddabb17783
Caio Santos <caio.santos@msn.com>
2023-04-19 23:57:18 +0900

    fix blogs.vue

--- src/store/index.js
+++ src/store/index.js
@@ -9,12 +9,6 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    sampleBlogCards: [
-      { blogTitle: 'Blog Card #1', blogCoverPhoto: 'stock-1', blogDate: 'January 20, 2023' },
-      { blogTitle: 'Blog Card #2', blogCoverPhoto: 'stock-2', blogDate: 'January 20, 2023' },
-      { blogTitle: 'Blog Card #3', blogCoverPhoto: 'stock-3', blogDate: 'January 20, 2023' },
-      { blogTitle: 'Blog Card #4', blogCoverPhoto: 'stock-4', blogDate: 'January 20, 2023' },
-    ],
     blogPosts: [],
     postLoaded: null,
     blogHTML: 'Write your blog title here...',
